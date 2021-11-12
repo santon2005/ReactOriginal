02cb0e08c9da32d37af3ea53a49d0faaa9ea6822
Santino Speranza <speranzasantino60@gmail.com>
2021-11-12 20:28:04 -0300

    clase03

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import './NavBar.css'
+
+const NavBar = () => {
+    return (
+       <ul>
+       <a href="#">HOME</a>
+       <a href="#">PRODUCT</a>
+       <a href="#">ABOUT</a>
+       <a href="#">CONTACT</a>
+        </ul>
+    );
+  }
+
+export default NavBar;
