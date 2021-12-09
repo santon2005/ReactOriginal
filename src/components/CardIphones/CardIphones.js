17d78b22dd28d9eec3f5cac3d8ec527604fc864f
Santino Speranza <speranzasantino60@gmail.com>
2021-12-09 18:54:54 -0300

    clase09

--- src/components/CardIphones/CardIphones.js
+++ src/components/CardIphones/CardIphones.js
@@ -5,7 +5,7 @@ import './CardIphones.css'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ItemCount from '../ItemCount/ItemCount';
-//import ItemDetailContainer from '../ItemDetailContainer/ItemDetailContainer';
+import itemDetalle from '../../views/ItemDetalle/itemDetalle';
 
 const CardIphones = ({Imagen, Nombre, Precio}) => {
     const alertProducts = () => {
@@ -25,7 +25,7 @@ const CardIphones = ({Imagen, Nombre, Precio}) => {
 </Card.Text>
 <Button variant="primary" onClick={alertProducts}>BUY</Button>
 <br />
-<Button variant="primary">MORE DETAILS</Button>
+<Button variant="primary" onClick={itemDetalle}>MORE DETAILS</Button>
 </Card.Body>
 <ToastContainer />
 <ItemCount/>
---
+++ src/views/ItemDetalle/itemDetalle.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ItemDetailContainer from '../../components/ItemDetailContainer/ItemDetailContainer'
+const itemDetalle = () => {
+    return (
+        <div>
+            <ItemDetailContainer />
+        </div>
+    )
+}
+
+export default itemDetalle
